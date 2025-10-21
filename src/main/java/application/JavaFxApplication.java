0a64680266f430fe52d1070513fb17249e9ae4f9
aleksander <aleksander.tapsi@voco.ee>
2025-10-21 12:35:35 +0300

    Button and label

--- src/main/java/application/JavaFxApplication.java
+++ src/main/java/application/JavaFxApplication.java
@@ -1,6 +1,10 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class JavaFxApplication extends Application {
@@ -8,6 +12,16 @@ public class JavaFxApplication extends Application {
     @Override
     public void start(Stage window) {
         window.setTitle("My first application");
+        Button button = new Button("This is a button");
+        Label textComponent = new Label("Text element");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+        componentGroup.getChildren().add(textComponent);
+
+        Scene scene = new Scene(componentGroup);
+
+        window.setScene(scene);
         window.show();
     }
 
