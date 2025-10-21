97d676312c2cc88a26b3e1d11aacd73679ad5892
aleksander <aleksander.tapsi@voco.ee>
2025-10-21 12:39:17 +0300

    added textField

--- src/main/java/application/JavaFxApplication.java
+++ src/main/java/application/JavaFxApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
@@ -14,10 +15,13 @@ public class JavaFxApplication extends Application {
         window.setTitle("My first application");
         Button button = new Button("This is a button");
         Label textComponent = new Label("Text element");
+        TextField textField = new TextField();
+        textField.setPromptText("Enter text");
 
         FlowPane componentGroup = new FlowPane();
         componentGroup.getChildren().add(button);
         componentGroup.getChildren().add(textComponent);
+        componentGroup.getChildren().add(textField);
 
         Scene scene = new Scene(componentGroup);
 
